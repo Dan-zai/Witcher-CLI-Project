06247d7ebd1cc6ae9df58c240f27ce6b55f04e91
Daniel Cowpertwait <danielcowpertwait@gmail.com>
2020-03-19 09:59:48 +0000

    Final Build!

--- lib/witcher/cli.rb
+++ lib/witcher/cli.rb
@@ -3,15 +3,39 @@ class Witcher::CLI
   def call
     Witcher::Chapters.scrape_chapters
     Witcher::Chapters.drop_last
-    puts "Welcome to the Bestiary, young Witcher! The finest collection of creatures and their information in all of The Continent".cyan.bold
+    puts "    ,   ,".magenta
+    puts "   /////|".magenta
+    puts "  ///// |".magenta
+    puts " /////  |".magenta
+    puts "|~~~| | |".magenta
+    puts "|===| |/|".magenta
+    puts "| B |/| |".magenta
+    puts "| E | | |".magenta
+    puts "| A | | |".magenta
+    puts "| S | | |".magenta
+    puts "| T | | |".magenta
+    puts "| I |/|/|".magenta
+    puts "| A | | |".magenta
+    puts "| R |  /".magenta
+    puts "| Y | /".magenta
+    puts "|===|/".magenta
+    puts "'---'".magenta
+    puts ""
+    
+    puts ""
+    puts "Welcome to the Bestiary, young Witcher! The finest collection of creatures and their attributes in all of The Continent".cyan.bold
+    puts ""
     menu
     goodbye
   end
 
   def menu
-    menu_prompt = "feel free to choose a chapter by it's numberor say farewell and I'll see you again soon!".cyan
+    menu_prompt = "Feel free to choose a chapter by it's numberor say farewell and I'll see you again soon!".cyan
+    puts ""
     puts "Here are our chapters on beasts and unnaturals".cyan
+    puts ""
     puts menu_prompt
+    puts ""
     display_chapters
     input = nil
     while input != "exit"
@@ -22,6 +46,7 @@ class Witcher::CLI
         # return index
         puts ""
         puts "#{chapter.name}! A fine choice, let's take a look at some examples".cyan
+        puts ""
         Witcher::Monster.scrape_monsters(chapter)
         monsters_in_chapter(chapter)
       elsif input == "farewell"
@@ -47,7 +72,7 @@ class Witcher::CLI
   end
 
   def monster_menu(chapter, monster)
-    menu_prompt = "Want to know about another #{chapter.name}?".cyan
+    menu_prompt = "Want to know about more #{chapter.name}?".cyan
     puts menu_prompt
     input = nil
     while input != "exit"
